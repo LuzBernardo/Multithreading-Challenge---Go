129a55f3c13675395b15fea4cdd5a3fc3255d5a8
Bernardo Luz <beluz.mario@gmail.com>
2024-02-04 16:56:03 -0300

    feat: create main file, main package and main func

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type response struct {
+	res *http.Response
+	api string
+}
+
+func handlerErr(err error, chanErr chan error) {
+	if err != nil {
+		chanErr <- err
+	}
+}
+
+func httpGet(client *http.Client, api string, errChan chan error, resChan chan response, service string) {
+	res, err := client.Get(api)
+	handlerErr(err, errChan)
+	response := response{
+		res: res,
+		api: service,
+	}
+	resChan <- response
+}
+
+func main() {
+	errChan := make(chan error)
+	resChan := make(chan response)
+	client := http.DefaultClient
+	cep := "01311200"
+
+	brasilApi := fmt.Sprintf("https://brasilapi.com.br/api/cep/v1/%s", cep)
+	viaApi := fmt.Sprintf("http://viacep.com.br/ws/%s/json", cep)
+
+	go func() {
+		for {
+			httpGet(client, viaApi, errChan, resChan, "viaApi")
+		}
+	}()
+	go func() {
+		for {
+			httpGet(client, brasilApi, errChan, resChan, "brasilApi")
+		}
+	}()
+
+	select {
+	case msg := <-resChan:
+		fmt.Printf("Chegou primeiro o: %s", msg.api)
+	case err := <-errChan:
+		log.Fatalf("%+v", err)
+	}
+}
